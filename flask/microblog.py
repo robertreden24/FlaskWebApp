3c21471c6f12f5296454634ff7d45892a2d80ef0
Rio Nugraha <43334190+rioadinugraha@users.noreply.github.com>
2020-06-13 20:12:14 +0700

    removed code that restarted making database everytime. run code and xampp to run make database

--- flask/microblog.py
+++ flask/microblog.py
@@ -2,8 +2,10 @@ from app import db,create_app
 from app.models import User,Post
 
 app = create_app()
-db.app = app
-db.create_all()
+#run this code in terminal with xampp run to create database
+# db.app = app
+# db.create_all()
+
 @app.shell_context_processor
 def make_shell_context():
     return{'db': db, 'User': User, 'Post': Post}
