e95cacc380e919e456cabd442c4434a6edd1faa4
Robert <robertreden2408@gmail.com>
2020-06-15 03:27:28 +0700

    add sochours to make event

--- flask/config.py
+++ flask/config.py
@@ -4,7 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI =  'mysql+pymysql://root:''@localhost/app.sql'
+    SQLALCHEMY_DATABASE_URI =  'mysql://newuser:password@localhost:3306/app'
+                                # 'mysql+pymysql://root:''@localhost/app.sql'
     #                          'mysql:///' + os.path.join(basedir, 'app.sql')os.environ.get('DATABASE_URI') or
 
     SQLALCHEMY_TRACK_MODIFICATIONS =False
