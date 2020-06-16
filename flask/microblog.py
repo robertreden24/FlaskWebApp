c6469091e65df19c31be855e8bbd7a8707191723
Robert <robertreden2408@gmail.com>
2020-06-16 20:56:41 +0700

    eb test

--- flask/microblog.py
+++ flask/microblog.py
@@ -1,10 +1,10 @@
 from app import db,create_app
 from app.models import User,Post
 
-app = create_app()
-#run this code in terminal with xampp run to create database
-# db.app = app
-# db.create_all()
+application = create_app()
+# run this code in terminal with xampp run to create database
+db.app = application
+db.create_all()
 
 @app.shell_context_processor
 def make_shell_context():
