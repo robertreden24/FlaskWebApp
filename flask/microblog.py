0fa5ca3c632a45bb1e268f0c4fcbbbebe1736562
Robert <robertreden2408@gmail.com>
2020-06-17 17:17:13 +0700

    change app

--- flask/microblog.py
+++ flask/microblog.py
@@ -1,10 +1,10 @@
 from app import db,create_app
 from app.models import User,Post
 
-app = create_app()
+application = create_app()
 # run this code in terminal with xampp run to create database
-db.app = app
-db.create_all()
+# db.app = app
+# db.create_all()
 
 @app.shell_context_processor
 def make_shell_context():
