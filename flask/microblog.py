5a351fd5b1634a718f93e02176d2d5b8485668da
robertreden24 <43334133+robertreden24@users.noreply.github.com>
2020-06-20 02:56:59 +0700

    Update microblog.py

--- flask/microblog.py
+++ flask/microblog.py
@@ -1,12 +1,12 @@
 from app import db,create_app
 from app.models import User,Post
 
-application = create_app()
+app = create_app()
 # run this code in terminal with xampp run to create database
 # db.app = app
 # db.create_all()
 
-@application.shell_context_processor
+@app.shell_context_processor
 def make_shell_context():
     return{'db': db, 'User': User, 'Post': Post}
 
