63abd04573b82c554c09e52cd7dee7bcf406bd19
robertreden24 <43334133+robertreden24@users.noreply.github.com>
2020-06-20 21:07:19 +0700

    Update test_flaskr.py

--- tests/test_flaskr.py
+++ tests/test_flaskr.py
@@ -3,32 +3,10 @@ import tempfile
 
 import pytest
 
-from app import db
+def pytest_configure(config):
+    import sys
+    sys._called_from_test = True
 
-@pytest.fixture
-def client():
-    db_fd, db.config['DATABASE'] = tempfile.mkstemp()
-    db.config['TESTING'] = True
-
-    with db.test_client() as client:
-        with db.app_context():
-            db.init_db()
-        yield client
-
-    os.close(db_fd)
-    os.unlink(db.config['DATABASE'])
-    
-# from flaskr import flaskr
-
-# @pytest.fixture
-# def client():
-#     db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-#     flaskr.app.config['TESTING'] = True
-
-#     with flaskr.app.test_client() as client:
-#         with flaskr.app.app_context():
-#             flaskr.init_db()
-#         yield client
-
-#     os.close(db_fd)
-#     os.unlink(flaskr.app.config['DATABASE'])
+def pytest_unconfigure(config):
+    import sys 
+    del sys._called_from_test
